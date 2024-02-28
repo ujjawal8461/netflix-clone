31dec7100eaa5692825d706e8eb8ed03692a6bbe
Ujjawal <ujjawal8461@gmail.com>
2024-02-29 00:01:16 +0530

    Data fetching in row completed

--- src/App.jsx
+++ src/App.jsx
@@ -9,11 +9,49 @@ import Row from "./Components/Row";
 function App() {
   return (
     <>
-      <Header />
-      <Row title="NETFLIX ORIGINALS" />
-      {/* <Row title="Trending Now" />
-      <Row title="Top Rated" />
-      <Row title="Most Popular" /> */}
+      <div className="bg-black">
+        <Header />
+        <Row
+          Category_title="NETFLIX ORIGINALS"
+          url={`https://api.themoviedb.org/3/discover/tv?api_key="`}
+          genre={``}
+        />
+        <Row
+          Category_title="Trending Now"
+          url={`https://api.themoviedb.org/3/trending/all/week?api_key=`}
+          genre={``}
+        />
+        <Row
+          Category_title="Top Rated"
+          url={`https://api.themoviedb.org/3/movie/top_rated?api_key=`}
+          genre={``}
+        />
+        <Row
+          Category_title="Most Popular"
+          url={`https://api.themoviedb.org/3/discover/movie?api_key=`}
+          genre={``}
+        />
+        <Row
+          Category_title="Comedy Movies"
+          url={`https://api.themoviedb.org/3/discover/movie?api_key=`}
+          genre={`&with_genres=35`}
+        />
+        <Row
+          Category_title="Horror Movies"
+          url={`https://api.themoviedb.org/3/discover/movie?api_key=`}
+          genre={`&with_genres=27`}
+        />
+        <Row
+          Category_title="Action Movies"
+          url={`https://api.themoviedb.org/3/discover/movie?api_key=`}
+          genre={`&with_genres=28`}
+        />
+        <Row
+          Category_title="Romance Movies"
+          url={`https://api.themoviedb.org/3/discover/movie?api_key=`}
+          genre={`&with_genres=10749`}
+        />
+      </div>
     </>
   );
 }
--- src/Components/Row.jsx
+++ src/Components/Row.jsx
@@ -1,22 +1,20 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 
-function Row({ title }) {
+function Row({ Category_title, url, genre }) {
   const API_KEY = "74b2bf9516944be552a12407fc7ba128";
+  const URL = `${url}${API_KEY}${genre}`;
+
+  const [MovieInfo, setMovieInfo] = useState([]);
 
   const getData = async () => {
     try {
-      const responce = await axios.get(
-        `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
-      );
+      const responce = await axios.get(URL);
       const movies = responce.data.results;
-      movies.forEach((movie) => {
-        console.log("Title:", movie.title);
-        console.log("poster:", movie.poster_path);
-        console.log("--------------");
-      });
+      console.log(MovieInfo);
+      setMovieInfo(movies);
     } catch (error) {
-      console.error("bhsi Dikkat aari he");
+      console.error("bhai Dikkat aari he");
     }
   };
 
@@ -26,7 +24,19 @@ function Row({ title }) {
 
   return (
     <>
-      <h2>{title}</h2>
+      <h2 className="text-white text-2xl font-bold mb-2">{Category_title}</h2>
+      <div className="my-8 overflow-x-auto whitespace-nowrap scrollbar-hidden">
+        <div className="flex gap-4 py-4 pr-4">
+          {MovieInfo.map((movie) => (
+            <img
+              key={movie.id}
+              src={`https://image.tmdb.org/t/p/w200/${movie.poster_path}`}
+              alt={movie.title}
+              className="w-24 h-36 object-cover rounded"
+            />
+          ))}
+        </div>
+      </div>
     </>
   );
 }
